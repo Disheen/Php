e48d75ca6e9148eaea09b7011871df5421ae4d99
Disheen Solanki <37015733+Disheen@users.noreply.github.com>
2020-04-22 16:36:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "DBMS";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
  /*
  if(isset($_POST["submit"])){
             $fname=$_POST["fname"];
