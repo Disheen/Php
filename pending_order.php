8cff2a84ca3766ef5690383c04e7938bd1f9977e
Disheen Solanki <37015733+Disheen@users.noreply.github.com>
2020-04-22 18:22:57 +0530

    Update pending_order.php

--- pending_order.php
+++ pending_order.php
@@ -169,7 +169,7 @@ li a:active  {
         <li><a  href="Manage.php">PLACE ORDER</a></li>
         <li><a  class="active" href="pending_order.php">PENDING ORDERS</a></li>
         <li><a href="completed_orders.php">COMPLETED ORDERS</a></li>
-         <li><a   href="Index.php">HOME</a></li><br><br><br>
+         <li><a   href="index.php">HOME</a></li><br><br><br>
         </ul>
         </nav>
 </body>
